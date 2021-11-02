b150f1d313b7ebf42fb2a6b68c960bb1fc591e13
shuo0112 <a0935953252@gmail.com>
2021-11-02 23:59:48 +0800

    edit

---
+++ src/java1104ch2/Sample2_15.java
@@ -0,0 +1,19 @@
+package java1104ch2;
+
+public class Sample2_15 {
+	public static void main(String[] args)
+	{
+		int[]test;
+		test = new int[5];
+				
+		test[0]	= 80;
+		test[1]	= 66;
+		test[2]	= 23;	
+		test[3]	= 51;	
+		test[4]	= 79;
+		
+		for(int i=0;i<5;i++) {	
+			System.out.println("第" + (i+1) + "個人的分數是" + test[i]+"分");
+		}
+	}
+}	
